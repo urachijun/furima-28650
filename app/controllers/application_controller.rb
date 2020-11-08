79af49b65cc695a44d5ff6033e5c6fda713b2ab7
urachijun <urachijun@icloud.com>
2020-11-08 16:54:23 +0900

    before_action作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :furima_auth
+
+  
+  private
+
+  def furima_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["FURIMA_AUTH_USER"] && password == ENV["FURIMA_AUTH_PASSWORD"]
+    end
+  end
 end
