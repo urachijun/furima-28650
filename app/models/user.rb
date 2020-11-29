405b4440d1daaf5a99f5d4fd5a2e127e52157e6c
urachijun <urachijun@icloud.com>
2020-11-29 10:17:12 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :nickname, :birthday
-    validates :email, format: { with: /@/, message: "is invalid. Input @ characters."}
+    validates :email, format: { with: /@/}
     validates :password, length: { minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze }
     validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
