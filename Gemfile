b976da04005bc8f2b23eaaa48b76bec72ccaf6e4
urachijun <urachijun@icloud.com>
2020-12-01 21:06:38 +0900

    Active Storegeの導入

--- Gemfile
+++ Gemfile
@@ -59,3 +59,6 @@ end
 
 gem 'pry-rails'
 gem 'devise'
+gem 'active_hash'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+private
+
+def item_params
+  params.require(:item).permit(:image).merge(user_id: current_user.id)
+end
+
 end
