63cc5e54be16ec02b88609e6d7d3b1a901731345
urachijun <urachijun@icloud.com>
2020-12-03 23:34:54 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
 belongs_to :user
 has_one_attached :image
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_fee_burden
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
 end
