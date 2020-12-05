079db88a4902f154fc9fce514f97c351305bac6c
urachijun <urachijun@icloud.com>
2020-12-05 17:24:28 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,10 @@ extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  with_options presence: true do
+    validates :name, :descriptionm, :price, :image
+    validates :category_id, :state_id, :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 0 }
+    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 }
+  end
+
 end
