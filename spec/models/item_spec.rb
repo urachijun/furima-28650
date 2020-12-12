5aeed697eac58f99f8e59cd879bc9067eef9efd5
urachijun <urachijun@icloud.com>
2020-12-12 11:19:15 +0900

    価格範囲設定のテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,12 +67,12 @@ RSpec.describe Item, type: :model do
       it 'priceが300未満では登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price must be greater than 300'
+        expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
       end
       it 'priceが10000000以上では登録できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price must be less than 10000000'
+        expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
     end
   end
