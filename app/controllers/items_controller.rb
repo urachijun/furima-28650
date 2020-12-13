cc4680fc799c5673158951689d03c19f27bffcb1
urachijun <urachijun@icloud.com>
2020-12-13 18:03:53 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-     @items = Item.all.order(created_at: :DESC)
-    
-    end
+    @items = Item.all.order(created_at: :DESC)
+  end
 
   def new
     @item = Item.new
