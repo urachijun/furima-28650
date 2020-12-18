a18e96dc916ca331955be3027056cf8a537d7119
urachijun <urachijun@icloud.com>
2020-12-18 15:26:35 +0900

    投稿者以外編集ページに遷移できない修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show, :edit, :update]
-
+  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -41,6 +41,12 @@ class ItemsController < ApplicationController
   
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
 end
