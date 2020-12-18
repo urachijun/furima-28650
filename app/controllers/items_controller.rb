206d0af09b60c31e81042565e11d3fc699ea516e
urachijun <urachijun@icloud.com>
2020-12-18 12:58:21 +0900

    同一処理のメソッド化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :DESC)
@@ -19,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -40,4 +38,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :descriptionm, :category_id, :state_id, :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
