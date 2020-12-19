9612d4f9ee7a0086a4c7a56673c569cb40fca6dd
urachijun <urachijun@icloud.com>
2020-12-19 11:05:10 +0900

    削除に関する重複記述削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     item.destroy
-    redirect_to action: :index if item.destroy
+    redirect_to action: :index
   end
 
   private
