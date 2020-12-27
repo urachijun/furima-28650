c19955dd856b4b283cbc7b6b239a8910efb1307c
urachijun <urachijun@icloud.com>
2020-12-27 11:06:54 +0900

    Formオブジェクトの作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :set_item
+
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+      if @purchase_address.valid?
+        @purchase_address.save
+        redirect_to items_path
+      else
+        render action: :index
+      end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id:current_user.id, item_id:params[:item_id] )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
