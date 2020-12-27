a9d11265c2c105822f9543a2ed254aa2be5c2315
urachijun <urachijun@icloud.com>
2020-12-27 11:08:03 +0900

    テーブルに保存できるようにするための修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,18 +1,18 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code,:prefecture,:city,:house_number,:building_name,:phone_number,:token,:user_id,:item_id 
+  attr_accessor :postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number,:user_id,:item_id 
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :token
+    validates :city, :house_number
   end
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :phone_number, numericality: { only_integer: true }, length: {maximum: 11}
   
   def save
     # 各テーブルにデータを保存する処理を書く
-    purchase = Parchase.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
