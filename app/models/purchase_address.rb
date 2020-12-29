1fd80343c33f0c10beeb82dd8a668c92c791c75b
urachijun <urachijun@icloud.com>
2020-12-29 21:12:56 +0900

    バリデーションwith_options範囲変更

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,10 +6,10 @@ class PurchaseAddress
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city, :house_number, :token, :user_id, :item_id
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
-
+  
   def save
     # 各テーブルにデータを保存する処理を書く
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
