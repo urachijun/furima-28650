687fcebeed477407ac65231d76e07b8f93acc82d
urachijun <urachijun@icloud.com>
2020-12-29 17:11:40 +0900

    トークンの情報が取得できるためストロングパラメータの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
       if @purchase_address.valid?
+        pay_item
         @purchase_address.save
         redirect_to items_path
       else
@@ -19,7 +20,7 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id:current_user.id, item_id:params[:item_id] )
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id:current_user.id, item_id:params[:item_id], token: params[:token] )
   end
 
   def set_item
@@ -28,6 +29,15 @@ class PurchasesController < ApplicationController
 
   def move_to_items_path
     redirect_to items_path if user_signed_in? && current_user.id == @item.user_id
+  end
+
+  def pay_item
+        Payjp.api_key = ENV["FURIMA_28650_SECRET_KEY"]
+        Payjp::Charge.create(
+          amount: @item[:price],
+          card: purchase_params[:token],
+          currency: 'jpy'
+        )
   end
 
 end
