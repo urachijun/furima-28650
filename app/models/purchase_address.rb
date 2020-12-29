1b8d04f750bec5f128929f52f51291499fb00848
urachijun <urachijun@icloud.com>
2020-12-29 17:14:54 +0900

    トークンのバリデーション

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,11 +1,11 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number,:user_id,:item_id 
+  attr_accessor :postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number,:user_id,:item_id, :token 
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, :house_number
+    validates :city, :house_number, :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :phone_number, numericality: { only_integer: true }, length: {maximum: 11}
